7c124db67aabc501a7bb407d6ecdf5ccd4816848
omaonyejiakor93 <omaonyejiakor@gmail.com>
2025-08-10 18:14:35 +0100

    Task 0: Added inline CSS styling to Header, UserProfile, MainContent, Footer components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', fontStyle: 'italic', color: '#333' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
