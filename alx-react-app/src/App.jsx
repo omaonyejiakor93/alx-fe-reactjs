ce0c61c58ef27f4d01d82bd63d8d9ebb0c431f30
omaonyejiakor93 <omaonyejiakor@gmail.com>
2025-08-10 15:21:39 +0100

    Task 1: Added WelcomeMessage component and integrated in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,13 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
