f30a1ac5a13480257697ea9d91df0da331615f30
omaonyejiakor93 <omaonyejiakor@gmail.com>
2025-08-11 05:13:38 +0100

    Fix Navbar inline styles: add backgroundColor, display, justifyContent to pass checker

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const navStyle = {
+    padding: '10px',
+    backgroundColor: '#eee',     // changed from background to backgroundColor
+    display: 'flex',             // added display
+    justifyContent: 'center',    // added justifyContent (you can change alignment)
+  };
+
+  const linkStyle = {
+    margin: '0 10px',
+  };
+
   return (
-    <nav style={{ padding: '10px', background: '#eee' }}>
-      <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
+    <nav style={navStyle}>
+      <Link to="/" style={linkStyle}>Home</Link>
+      <Link to="/about" style={linkStyle}>About</Link>
+      <Link to="/services" style={linkStyle}>Services</Link>
+      <Link to="/contact" style={linkStyle}>Contact</Link>
     </nav>
   );
 }
