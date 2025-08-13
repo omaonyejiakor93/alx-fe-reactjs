3f14a9bf6f674ccf82459f570f1c387626fa0693
omaonyejiakor93 <omaonyejiakor@gmail.com>
2025-08-13 05:53:39 +0100

    Fix: add BrowserRouter in App.jsx for checker compliance

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,40 +1,40 @@
 // src/App.jsx
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/recipes" style={{ marginLeft: "10px" }}>
-          Recipes
-        </Link>
-      </nav>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/recipes">Recipes</Link>
+        </nav>
 
-      <Routes>
-        {/* Home route */}
-        <Route
-          path="/"
-          element={
-            <div>
-              <h1>My Recipe App</h1>
-              <AddRecipeForm />
-              <RecipeList />
-            </div>
-          }
-        />
+        <Routes>
+          {/* Home route */}
+          <Route
+            path="/"
+            element={
+              <div>
+                <h1>My Recipe App</h1>
+                <AddRecipeForm />
+                <RecipeList />
+              </div>
+            }
+          />
 
-        {/* Recipes list route */}
-        <Route path="/recipes" element={<RecipeList />} />
+          {/* Recipes list route */}
+          <Route path="/recipes" element={<RecipeList />} />
 
-        {/* Recipe details route */}
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+          {/* Recipe details route */}
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
