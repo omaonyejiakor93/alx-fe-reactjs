5662b1be273c642437647787cadef6b87357aa03
omaonyejiakor93 <omaonyejiakor@gmail.com>
2025-08-13 03:46:08 +0100

    Fix: addded setRecipes to recipe Store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 
+// Create a Zustand store for recipes
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) =>
-    set((state) => ({ recipes: [...state.recipes, recipe] })),
+
+  // Function to update the recipes array
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
 
 export default useRecipeStore;
